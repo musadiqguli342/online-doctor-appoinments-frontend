36d09e5cc75aa1f3d58b19f474578ea2421d84f9
Seema <musadiqguli342@gmail.com>
2026-02-18 22:49:32 -0800

    Fix BookAppointment route

---
+++ app/book-appointment/page.tsx
@@ -0,0 +1,11 @@
+// page.tsx
+import { Suspense } from "react";
+import BookAppointmentClient from "./BookAppointmentClient/page";
+
+export default function Page() {
+  return (
+    <Suspense fallback={<div className="text-center mt-5">Loading...</div>}>
+      <BookAppointmentClient />
+    </Suspense>
+  );
+}
